09d0eb7e6274d9484591ce89d9a77481078b7b66
1King_coder <progvitor@gmail.com>
2023-09-08 01:14:50 -0300

    Numeric Calculus study and visualization method

---
+++ Visualizando_metodos_para_zeros.py
@@ -0,0 +1,65 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
+from calculando_zeros import Func
+import sympy as sym
+from math import sin, cos
+"""
+
+figure, axe = plt.subplots()
+axe.set(xlim=[-10, 20], ylim=[-0.5, 50], xlabel='X', ylabel='Y')
+
+x = np.arange(-10, 10, 0.1)
+
+f = (x - 2)*(x - 3)
+
+func = axe.plot(x, f, label="f(x)")
+
+f_line = [axe.plot(x, (2*i - 5)*(x - i) + (i-2)*(i-3), c='r', label="f'(x)") for i in x]
+f_scatter = [axe.plot(i, (i - 2)*(i - 3), c='r',marker='.') for i in x]
+
+"""
+"""
+def update_frame (frame) -> tuple:
+    x_axe = x[:frame]
+    y = g[:frame]
+    
+    tg_line.set_xdata(x_axe)
+    tg_line.set_ydata(y)
+
+    
+
+    return [tg_line]
+
+anime = animation.FuncAnimation(figure, update_frame, 1000, interval=10, blit=True)
+"""
+"""
+derivative_line_animation = animation.ArtistAnimation(fig=figure, artists=f_line, interval=0)
+derivative_dot_animation = animation.ArtistAnimation(fig=figure, artists=f_scatter, interval=0)
+"""
+
+x = sym.symbols('x')
+func = Func((x - 2)*(x - 3), x)
+
+res, iter_x, iter_y = func.newton_method(0, 10**(-6)).values()
+
+iter_x, iter_y = [float(value) for value in iter_x], [float(value) for value in iter_y]
+
+x_np = np.arange(-10, 10, 0.1)
+print (type(x_np))
+
+fig, axe = plt.subplots()
+axe.set(xlim= [-1, 4], ylim= [-0.25, 1])
+
+axe.plot(x_np, func.f(x_np))
+axe.plot(x_np, [0 for  _ in x_np], c='black', label='x')
+axe.plot([0 for _ in x_np], x_np, c='black', label='y')
+
+derivative = [axe.plot(x_np, (x_np - i)*func.f(i, func.dfunc) + func.f(i), c='r') for i in iter_x]
+where_derivative_touches_x = [axe.plot(i, 0, c='orange', marker='.') for i in iter_x[1:]]
+
+axe.plot(res, 0, c='r', marker='.')
+anim = animation.ArtistAnimation(fig, derivative, interval=0)
+
+anim_2 = animation.ArtistAnimation(fig, where_derivative_touches_x, interval=0)
+plt.show()
---
+++ calculando_zeros.py
@@ -0,0 +1,154 @@
+from math import ceil, log
+from decimal import getcontext, Decimal
+from random import randrange
+import sympy as sym
+import numpy as np
+
+prec = 6
+getcontext().prec = prec
+
+def decimal (num: float) -> 'Decimal':
+    # float to decimal conversor
+    return Decimal(str(num))
+
+class Func:
+    """
+    Class made to analise mathematical functions with sympy in a intuitive way
+    and make it possible to visualize them in a graphic way
+    """
+    def __init__ (self, func: sym.core.add.Add, x: sym.core.symbol.Symbol) -> None:
+        self.func = func
+        self.x = x
+
+    def f (self, value, function=None):
+        # Makes getting the function value from a given X easy
+        if isinstance(value, np.ndarray):
+            # Checks if the value is a ndarray and converts the results to
+            # an array, making the use of it with matplotlib possible and easier
+            return [(function if function else self.func).subs({self.x: i}) for i in value]
+
+        return (function if function else self.func).subs({self.x: value})
+    
+    @property
+    def dfunc (self):
+        # 1º derivative of the given function
+        return self.func.diff()
+    
+    @property
+    def ddfunc (self):
+        # 2º derivative of the given function
+        return self.dfunc.diff()
+    
+    @staticmethod
+    def _d (function: sym.core.add.Add):
+        # Derivates a given function
+        return function.diff()
+
+    def verify_derivate (self, a):
+        # Verify if the derivate of the function in x=a is 0
+        if self.dfunc == 0:
+            print('Derivative = 0')
+            return False
+        
+        return True
+
+    def verify_zero (self, a: 'Decimal', b: 'Decimal') -> bool:
+        # Verify if there is a 0 in a given interval
+
+        if self.f(a)*self.f(b) > 0:
+            print("Não há 0 da função no intervalo ou intervalo inválido para análise!")
+            return False
+        
+        return True
+
+    def bissection_method (self, a: float, b: float, TOL: float) -> dict:
+        """
+        Uses the bissection method to find the solution of the function
+        with TOL's precision.
+        Returns a dictionary with the result and lists with the x and y
+        coordinates used in the method.
+        """
+        a, b, TOL = decimal(a), decimal(b), decimal(TOL)
+
+        Max_iterations = ceil(
+            (decimal(log(b - a)) - decimal(log(TOL)))/decimal(log(2)))
+
+        print(f'Num. Maximo de iterações: {Max_iterations}')
+
+        if not self.verify_zero(a, b):
+            return False
+        
+        iteration = 0
+        x = decimal((a + b)/2)
+        iterations_x = []
+        iterations_y = []
+
+        while iteration < Max_iterations:
+
+            x = decimal((a + b)/2)
+
+            if self.f(x) == 0 or decimal((b-a)/2) < TOL:
+                break
+            
+            if self.f(x)*self.f(a) < 0:
+                b = x
+            else:
+                a = x
+
+            
+            iteration += 1
+            iterations_x.append(x)
+            iterations_y.append(self.f(x))
+
+        return {'result': x, 'iter_x': iterations_x, 'iter_y': iterations_y}
+    
+    def newton_method (self, a: float, TOL: float):
+        """
+        Uses the Newton's method to find the solution of the function 
+        with TOL precision.
+        Returns a dictionary with the result and lists with the x and y
+        coordinates used in the method.
+        """
+
+        a, TOL = decimal(a), decimal(TOL)
+
+        if not self.verify_derivate(a):
+            return False
+        
+        x = a - self.f(a)/self.f(a, self.dfunc)
+        iter_x = []
+        iter_y = []
+        
+        while not abs(self.f(x)) < TOL:
+
+            if not self.verify_derivate(x):
+                print(x)
+                return False
+
+            if self.f(x) == 0:
+                break
+
+            x -= self.f(x)/self.f(x, self.dfunc)
+
+            iter_x.append(x)
+            iter_y.append(self.f(x))
+
+            
+
+        return {'result': x, 'iter_x': iter_x, 'iter_y': iter_y}
+        
+
+        
+
+
+
+
+if __name__ == '__main__':
+    # tests
+    lista = [1, 2, 3, 4]
+
+    ...
+
+
+
+
